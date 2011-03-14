ea0b9c84fb1ca6f25a2220364a93d076d809e9f7
Claus Bönnhoff <info@cbc-apps.com>
2011-03-14 14:24:56 +0100

    v1.2

--- ColorPicker/Classes/ColorPickerViewController.h
+++ ColorPicker/Classes/ColorPickerViewController.h
@@ -9,18 +9,22 @@
 #import <UIKit/UIKit.h>
 #import "ColorPicker.h"
 
+// we need to be Delegate for the ColorPicker as well as for the PopoverController
 @interface ColorPickerViewController : UIViewController <UIPopoverControllerDelegate,ColorPickerDelegate>
 {
-    UIPopoverController *popoverController;
-    UIButton *changeColorButton;
-    UIColor *color;
+    UIPopoverController *popoverController; // for iPad device
+    UIButton *changeColorButton;            // IBOutlet Button
+    UIColor *color;                         // our color 
 }
 
 @property (nonatomic, retain) UIPopoverController *popoverController;
 @property (nonatomic, retain) IBOutlet UIButton *changeColorButton;
 @property (nonatomic, retain) UIColor *color;
 
+// IBAction for changeColorButton touch up inside
 -(IBAction)changeColor:(id)sender;
+
+// the needed delegate methods for the ColorPicker
 
 - (void)colorPicker:(ColorPicker *)colorPicker didSelectColorWithTag:(NSInteger)tag Red: (NSUInteger)red Green:(NSUInteger)green Blue:(NSUInteger)blue Alpha:(NSUInteger)alpha;
 - (void)colorPicker:(ColorPicker *)colorPicker didCancelWithTag:(NSInteger)tag;
--- ColorPicker/Classes/ColorPickerViewController.m
+++ ColorPicker/Classes/ColorPickerViewController.m
@@ -13,6 +13,7 @@
 
 @synthesize popoverController,changeColorButton,color;
 
+// take a look on which device we are running
 -(BOOL)deviceIsiPad
 {
     NSString *devstr=[[UIDevice currentDevice] model];
@@ -21,28 +22,38 @@
     return NO;
 }
 
+// changeColorbutton is pressed
 -(IBAction)changeColor:(id)sender
 {
+    // iPad ?
     if([self deviceIsiPad])
     {
+        // create the ColorPicker with iPad layout
         ColorPicker *picker=[[ColorPicker alloc] initWithNibName:@"ColorPicker_iPad" bundle:nil Tag:1 Color:color];
         if(picker)
         {
+            // set delegate
             picker.delegate=self;
+            // and create PopoverController
 			popoverController=[[UIPopoverController alloc] initWithContentViewController:picker];
 			popoverController.delegate=self;
 			[popoverController setPopoverContentSize:CGSizeMake(430, 435)];
 			picker.popOver=popoverController;
+            // and show it next to our Button
 			[popoverController presentPopoverFromRect:changeColorButton.frame inView:self.view permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
 		}
         [picker release], picker=nil;
     }
+    // iPhone
 	else
 	{
+        // create the ColorPicker with iPhone layout
         ColorPicker *picker=[[ColorPicker alloc] initWithNibName:@"ColorPicker" bundle:nil Tag:1 Color:color];
         if(picker)
         {
+            // set delegate
             picker.delegate=self;
+            // and present it as a modal view
             [self presentModalViewController:picker animated:YES];
         }
         [picker release], picker=nil;
@@ -52,14 +63,19 @@
 #pragma mark -
 #pragma mark Color picker delegate methods
 
+// the delegate mehtods
+
+// just to dismiss the PopoverController
 - (void)popoverControllerDidDismissPopover:(UIPopoverController *)popController
 {
     [popController dismissPopoverAnimated:YES];
 	self.popoverController=nil;
 }
 
+// ColorPicker did cancel
 - (void)colorPicker:(ColorPicker *)colorPicker didCancelWithTag:(NSInteger)userTag 
 {
+    // act on device and clear Popover or Modal controller
     if([self deviceIsiPad])
     {
         [self popoverControllerDidDismissPopover:popoverController];
@@ -70,12 +86,14 @@
     return;
 }
 
-
+// ColorPicker has done with OK Button
 - (void)colorPicker:(ColorPicker *)colorPicker didSelectColorWithTag:(NSInteger)userTag Red:(NSUInteger)red Green:(NSUInteger)green Blue:(NSUInteger)blue Alpha:(NSUInteger)alpha
 {
+    // here we can act different depending on the userTag
 	switch(userTag)
     {
         case 1:
+            // we just set our Color. Color comes from the ColorPicker in RGBA 0-255 Values
             self.color=[UIColor colorWithRed:(float)red/255.0 green:(float)green/255.0 blue:(float)blue/255.0 alpha:(float)alpha/255.0];
             self.view.backgroundColor=color;
             break;
@@ -83,6 +101,7 @@
             break;
     }
     
+    // dismiss the Picker
     if([self deviceIsiPad])
     {
         [self popoverControllerDidDismissPopover:popoverController];
